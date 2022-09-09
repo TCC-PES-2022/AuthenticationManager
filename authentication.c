9c9307e6e2ed934edd1ea6a40069655e54b190a4
otaviocomp <otavio10espsanto@gmail.com>
2022-09-08 21:16:29 -0300

    sanitize user and password

--- authentication.c
+++ authentication.c
@@ -18,7 +18,8 @@ static Authentication_status sanitizePassword(char *);
 /* FUNCTION DEFINITIONS */
 /************************/
 
-Authentication_status signUp(char *user, char *password)
+Authentication_status
+signUp(char *user, char *password)
 {
 	Authentication_status status;
 
@@ -28,7 +29,8 @@ Authentication_status signUp(char *user, char *password)
 	return status;
 }
 
-Authentication_status authentication(char *password)
+Authentication_status
+authentication(char *password)
 {
 	Authentication_status status;
 
@@ -37,7 +39,8 @@ Authentication_status authentication(char *password)
 	return status;
 }
 
-static Authentication_status sanitizeUser(char *user)
+static Authentication_status
+sanitizeUser(char *user)
 {
 	regex_t regex_specialchar, regex_capital;
 
@@ -46,16 +49,27 @@ static Authentication_status sanitizeUser(char *user)
 		return AU_SIGN_UP_ERROR;
 
 	/* compile regex */
-	if (regcomp(&regex_specialchar, "#@%*-=+", 0))
+	if (regcomp(&regex_specialchar, "#@%*-=+$", REG_EXTENDED))
 		return AU_ERROR;
 	if (regcomp(&regex_capital, "[A-Z]", 0))
 		return AU_ERROR;
+
+	/* execute regular expression */
+	if (regexec(&regex_specialchar, user, 0, NULL, 0))
+		return AU_SIGN_UP_ERROR;
+	if (regexec(&regex_capital, user, 0, NULL, 0))
+		return AU_SIGN_UP_ERROR;
+
+	/* free regex */
+	regfree(&regex_specialchar);
+	regfree(&regex_capital);
 
 	/* sign up with success */
 	return AU_SIGN_UP_OK;
 }
 
-static Authentication_status sanitizePassword(char *password)
+static Authentication_status
+sanitizePassword(char *password)
 {
 	regex_t regex_specialchar, regex_capital;
 
@@ -63,11 +77,21 @@ static Authentication_status sanitizePassword(char *password)
 	if (strlen(password) < MIN_LEN_PW || strlen(password) > MAX_LEN_PW)
 		return AU_SIGN_UP_ERROR;
 
-	/* compile regex */
-	if (regcomp(&regex_specialchar, "#@%*-=+", 0))
+	/* compile regular expression */
+	if (regcomp(&regex_specialchar, "#@%*-=+$", REG_EXTENDED))
 		return AU_ERROR;
 	if (regcomp(&regex_capital, "[A-Z]", 0))
 		return AU_ERROR;
+	
+	/* execute regular expression */
+	if (!regexec(&regex_specialchar, password, 0, NULL, 0))
+		return AU_SIGN_UP_ERROR;
+	if (regexec(&regex_capital, password, 0, NULL, 0))
+		return AU_SIGN_UP_ERROR;
+
+	/* free regex */
+	regfree(&regex_specialchar);
+	regfree(&regex_capital);
 
 	/* sign up with success */
 	return AU_SIGN_UP_OK;
