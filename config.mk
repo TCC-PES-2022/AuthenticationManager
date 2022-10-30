c8e82f1b0ebc710394bddc458514b29be327e58d
Kollins <kgl2@cin.ufpe.br>
2022-10-30 15:57:03 -0300

    Remove optimization flags

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 BIN        = login
 CC         = cc
 AR         = ar
-CFLAGS     = -Wall -Wextra -O3 -march=native -fPIC
+CFLAGS     = -Wall -Wextra -fPIC
 COVFLAGS   = -fprofile-arcs -ftest-coverage -g
 LDFLAGS    = -lgcrypt -lgpg-error
 COVLDFLAGS = -lgcov --coverage
